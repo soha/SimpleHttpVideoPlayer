c178592b618c103f2d6908ddd0ad3a64d83796f2
sohatach <sohatach+git@gmail.com>
2011-07-18 17:09:31 +0900

    first commit.

---
+++ src/jp/sohatach/SimpleHttpVideoPlayerActivity.java
@@ -0,0 +1,142 @@
+package jp.sohatach;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class SimpleHttpVideoPlayerActivity extends Activity {
+	private String mediaPath;
+	private VideoView videoView;
+	public static final int WEB_REQUEST_CODE = 0;
+	public static final int RESULT_OK = 0;
+	
+	/**
+	 * 動画ファイルのURL直接指定
+	 */
+	private static final int VIDEO_URL = 1;
+	/**
+	 * 動画ファイルへのリンクを含むWebページの指定
+	 */
+	private static final int WEB_URL = 2;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		videoView = (VideoView) findViewById(R.id.videoView);
+		
+		//mediaPath = "http://www.gomplayer.jp/support/down/mp4_mpeg4_aac.mp4";
+		//mediaPath = "/sdcard/mpeg4.mp4";
+//
+//		if (mediaPath != null) {
+//			playVideo();
+//		}
+	}
+
+	/**
+	 * Videoの再生
+	 */
+	private void playVideo() {
+		videoView.setVideoPath(mediaPath);
+		videoView.setMediaController(new MediaController(this));
+		videoView.requestFocus();
+		videoView.start();
+	}
+
+	
+	/**
+	 * オプションメニューの作成
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean ret = super.onCreateOptionsMenu(menu);
+
+		menu.add(0 , Menu.FIRST , Menu.NONE , "URL入力");
+		menu.add(0 , Menu.FIRST + 1 ,Menu.NONE , "Webから選択");
+
+		return ret;
+	}
+	
+	/**
+	 * オプションメニュー選択時の処理
+	 */
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == Menu.FIRST){
+			//URL入力
+			showInputVideoUrlDialog();
+		}else{
+			//WebURL入力
+			showInputWebUrlDialog();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+	/**
+	 * 動画URL入力のためのダイアログ表示
+	 */
+	private void showInputVideoUrlDialog() {
+		_showDialog(VIDEO_URL);
+	}
+	
+	/**
+	 * 動画ファイルへのリンクを持つWebページのURL入力のためのダイアログ表示
+	 */
+	private void showInputWebUrlDialog() {
+		_showDialog(WEB_URL);
+	}
+	
+	/**
+	 * URL入力のためのダイアログ表示
+	 */
+	private void _showDialog(int type) {
+	    final EditText editText = new EditText(this);
+	    final int inputType = type;
+
+	    final AlertDialog alertDialog = new AlertDialog.Builder(this)
+	        .setTitle("input URL")
+	        .setView(editText)
+	        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					String url = editText.getText().toString().trim();
+					if("".equals(url)){
+						return;
+					}
+					if(VIDEO_URL == inputType) {
+						mediaPath = url;
+						playVideo();
+					}else{
+						Intent i = new Intent(SimpleHttpVideoPlayerActivity.this, WebInputActivity.class);
+						i.putExtra("web_url", url);
+						//startActivity(i);
+						startActivityForResult(i, WEB_REQUEST_CODE);
+						//webView.loadUrl(url);
+					}
+				}
+			}).setNegativeButton("Cancel", null)
+	        .create();
+	    alertDialog.show();
+	}
+	
+	/**
+	 * Web入力画面からの戻り処理
+	 */
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(WEB_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
+			if(data != null) {
+				String url = data.getStringExtra("video_url");
+				if(url != null && !url.trim().equals("")) {
+					mediaPath = url;
+					playVideo();
+				}
+			}
+		}
+	}
+}
---
+++ src/jp/sohatach/WebInputActivity.java
@@ -0,0 +1,40 @@
+package jp.sohatach;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebInputActivity extends Activity {
+	private WebView webView;
+	private String web_url;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.web_input);
+		
+		webView = (WebView) findViewById(R.id.webView);
+		webView.setWebViewClient(new MyWebViewClient());
+		
+		Intent i = getIntent();
+		if(i != null) {
+			web_url = i.getExtras().getString("web_url");
+			webView.loadUrl(web_url);
+		}
+	}
+
+	private class MyWebViewClient extends WebViewClient {
+	    @Override
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	    	
+	    	Intent i = new Intent();
+	    	i.putExtra("video_url", url);
+	    	setResult(SimpleHttpVideoPlayerActivity.RESULT_OK, i);
+	    	finish();
+	    	
+	        return true;
+	    }
+	}
+}
